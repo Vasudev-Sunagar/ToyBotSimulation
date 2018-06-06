bf94eabe7e2a385a615373246401515155d3d5cb
vasudev.sunagar <vasudev.sunagar@robosoftin.com>
2018-06-06 15:27:56 +0530

    Initial commit, folder structure and related files

---
+++ ToyBotSimulation/PlaceInstructionArguments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyBotSimulation
+{
+    class PlaceInstructionArguments
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Facing Facing { get; set; }
+    }
+}
---
+++ ToyBotSimulation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyBotSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayWelcome();
+
+            var driver = new RobotDriver(new Robot());
+
+            while (true)
+            {
+                string command = PromptForCommand();
+                if (command.ToUpper() == "EXIT" || command.ToUpper() == "QUIT")
+                {
+                    Environment.Exit(0);
+                }
+                Console.WriteLine(driver.Command(command));
+            }
+        }
+
+        private static void DisplayWelcome()
+        {
+            Console.WriteLine("Toy Robot Simulation");
+            Console.WriteLine("---------");
+            Console.WriteLine("");
+        }
+
+        private static string PromptForCommand()
+        {
+            Console.WriteLine("#: ");
+            return Console.ReadLine();
+        }
+
+
+    }
+}
